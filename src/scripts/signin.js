c608c802b3a3fbad6637a9ae3230b63d473b5ea0
mhmdawada1 <mohammadawada589@gmail.com>
2023-07-29 04:26:08 +0300

    creating javascript methods for sign in

---
+++ src/scripts/signin.js
@@ -0,0 +1,27 @@
+const login = document.getElementById("login-btn");
+
+login.addEventListener("click", async (e) => {
+  e.preventDefault();
+  email = document.getElementById("email-in").value;
+  password = document.getElementById("password-in").value;
+
+  const data = new FormData();
+  data.append("email",email);
+  data.append("password",password);
+  try{
+    const response = await fetch(,{
+      method : POST,
+      body : data
+    });
+    const res = await response.json();
+
+    if (res.status == "next step"){
+      window.location.href = "/src/pages/landingpage.html";
+    }
+    else {
+      document.querySelector(".wrong-password").innerHTML = "Invalid username or password!";
+    }
+  }catch(error) {
+    console.log(error);
+  }
+})
